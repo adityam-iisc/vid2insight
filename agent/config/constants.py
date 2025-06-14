25a150835bfbf7fc333212364d7fdc7a1f7950e2
Trupti <krishnatsm18@gmail.com>
2025-06-14 13:15:00 +0530

    frame transcript generator added

--- agent/config/constants.py
+++ agent/config/constants.py
@@ -2,3 +2,4 @@ import os
 
 PROVIDER = os.getenv('MODEL_PROVIDER', 'google_genai')
 MODEL_NAME = os.getenv('MODEL_NAME', 'gemini-2.0-flash')
+
--- ingestion/frame_transcript_generator.py
+++ ingestion/frame_transcript_generator.py
@@ -1,5 +1,11 @@
+import json
+
 import cv2
+from langchain_core.language_models import BaseChatModel
 from langchain_core.runnables import RunnableConfig
+
+import re
+import agent
 from agent.config.initialize_logger import logger
 
 from agent.config.assistant_config import AssistantConfiguration
@@ -10,10 +16,11 @@ import cv2
 import base64
 from typing import Dict, List, Union
 from agent.doc_agent import prompts
+from ingestion import constants
 from ingestion.frame_json_parser import FrameJsonOutputParser
 
 
-def generate_frame_segment_transcript(path_to_frame_folder: str): #-> Dict[str, str]:
+def generate_frame_segment_transcript(path_to_frame_folder: str) -> list[dict]:
     """
     Generate a frame transcript based on the agent's state and configuration.
 
@@ -34,31 +41,12 @@ def generate_frame_segment_transcript(path_to_frame_folder: str): #-> Dict[str, 
         # LLM Configuration
         logger.info("---GENERATE FRAME TRANSCRIPT FOR INGESTION---")
         dict = read_frames_from_folder(path_to_frame_folder)
-        llm_msg = request_llm(dict)
+        llm_msg = get_img_content_list(dict)
         configuration = AssistantConfiguration()
         logger.info(configuration.default_llm_model['provider'])
         logger.info(configuration.default_llm_model['model_name'])
         chat_model = configuration.get_model(configuration.default_llm_model)
-
-        # Prepare prompts and messages
-        messages = [
-            HumanMessage(content=[
-                {"type": "text", "text": prompts.FRAME_EXTRACT_PROMPT_2},
-                *llm_msg  # Add all images as content parts
-            ])
-        ]
-
-        # Generate the frame transcript
-        frame_transcript = chat_model.invoke(messages)
-        #logger.debug(f"Generated frame transcript: {frame_transcript.content}")
-
-        # Use the parser
-        parser = FrameJsonOutputParser()
-        parsed_output = parser.parse(frame_transcript.content)
-
-        # Access example
-        print(parsed_output[0]["summary"]["title"])
-
+        llm_requests(chat_model)
     except Exception as exc:
         logger.exception(f"Exception in creating transcription of frame segments: {exc}")
         raise
@@ -68,14 +56,92 @@ def read_frames_from_folder(path_to_frame_folder) -> Dict[str, str]:
     img_base64_dict = {}
     for entry in os.scandir(directory):
         if entry.is_file():  # check if it's a file
-            print(entry.name)
+            logger.info(entry.name)
             img_path = os.path.join(directory, entry.name)
             with open(f"{img_path}", "rb") as imagefile:
                 convert = base64.b64encode(imagefile.read()).decode('utf-8')
             img_base64_dict[f"{entry.name}"] = f"{convert}"
     return img_base64_dict
 
-def request_llm(base64_img : Dict[str,str]):
+def llm_requests(chat_model):
+    """
+    Create a list of LLM requests from the base64 encoded images.
+
+    Returns:
+        List[Dict[str, str]]: A list of dictionaries containing image data for LLM requests.
+    """
+    req_output_list = []
+
+    req_parts = [{"type": "text", "text": prompts.FRAME_EXTRACT_PROMPT_2} ]
+    current_payload_size = (
+        len(json.dumps(req_parts).encode("utf-8")))
+
+    # Read frames from the folder and convert to base64
+    base64_img = read_frames_from_folder("../docs/frames")
+
+    # Create LLM requests from the base64 images
+    img_list = get_img_content_list(base64_img)
+
+    is_llm_call_pending = True
+    for img, img_name in zip(img_list, base64_img.keys()):
+        path = img_name
+        projected_size = current_payload_size + len(json.dumps(img).encode("utf-8"))
+        if projected_size > constants.MAX_PAYLOAD_BYTES:
+            logger.info(f"Batch {path} – would exceed 19 MB limit.")
+            req_output = get_llm_response(req_parts, chat_model)
+            for req_op in req_output:
+                req_output_list.append(req_op)
+            logger.info("Request parts sent to LLM: {}",len(req_output_list))
+            is_llm_call_pending = False
+
+            # Reset the request parts and add the new image
+            logger.info("Resetting request parts for the next batch.")
+            req_parts = {"type": "text", "text": prompts.FRAME_EXTRACT_PROMPT_2}
+            current_payload_size = (
+                len(json.dumps(req_parts).encode("utf-8")))
+            break
+        else:
+            is_llm_call_pending = True
+            req_parts.append(img)
+            current_payload_size = projected_size
+            logger.info(f"Added {path} – current payload size: {current_payload_size / (1024 * 1024):.2f} MB")
+    logger.info(f"Final payload size: {current_payload_size / (1024 * 1024):.2f} MB")
+    if is_llm_call_pending:
+        logger.info("Sending final request parts to LLM.")
+        req_output = get_llm_response(req_parts, chat_model)
+        for req_op in req_output:
+            # print(req_op)
+            req_output_list.append(req_op)
+        logger.info("Request parts sent to LLM")
+
+
+    return req_output_list
+
+
+
+def get_llm_response(req_parts: List[Dict[str, str]], chat_model: BaseChatModel) -> list[dict]:
+    # Prepare prompts and messages
+    messages = [
+        HumanMessage(content=[
+            *req_parts  # Add all images as content parts
+        ])
+    ]
+    # Generate the frame transcript
+    frame_transcript = chat_model.invoke(messages)
+    logger.debug(f"Generated frame transcript: {frame_transcript.content}")
+    print(f"Generated frame transcript: {frame_transcript.content}")
+    # Use the parser
+    parser = FrameJsonOutputParser()
+
+    parsed_output = parser.parse(frame_transcript.content)
+
+    # Access example
+    # logger.info("Parsed Output: ", parsed_output)
+    #logger.info("Summary Title: " ,parsed_output[0]["summary"]["title"])
+    return parsed_output
+
+
+def get_img_content_list(base64_img : Dict[str,str]):
     img_list = []
     for key in base64_img:
         img_list.append(
