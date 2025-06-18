e3c947bf5b84e9c07b63524c731ed0c9faf0bfb5
ankit <ankitwave10@gmail.com>
2025-06-18 17:51:24 +0530

    Deleted eval folder

--- eval/config/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Configuration modules for evaluation."""
-
-from eval.config.model_config import LLM_MODELS, DEFAULT_LLM_MODEL, BERT_CONFIG, BLEU_CONFIG
-
-__all__ = ['LLM_MODELS', 'DEFAULT_LLM_MODEL', 'BERT_CONFIG', 'BLEU_CONFIG']
