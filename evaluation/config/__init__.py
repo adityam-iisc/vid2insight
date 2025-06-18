1b0f8331dd7bbd5a2bfe7851519efe272c8421c0
ankit <ankitwave10@gmail.com>
2025-06-18 17:48:15 +0530

    updated few evaulation parameter

---
+++ evaluation/config/__init__.py
@@ -0,0 +1,5 @@
+"""Configuration modules for evaluation."""
+
+from evaluation.config.model_config import DEFAULT_LLM_MODEL, BERT_CONFIG, BLEU_CONFIG
+
+__all__ = ['DEFAULT_LLM_MODEL', 'BERT_CONFIG', 'BLEU_CONFIG']
